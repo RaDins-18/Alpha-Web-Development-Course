3ef6455a884507bf6ddab62bc289171b30a28b14
RaDin <143905822+RaDins-18@users.noreply.github.com>
2024-07-18 07:03:04 -0700

    class_91

--- 91 Day_91 Exercise 15 - Clear the Clutter/main.js
+++ 91 Day_91 Exercise 15 - Clear the Clutter/main.js
@@ -23,16 +23,10 @@
 // SOLUTION:
 
 // Import necessary modules.
+import fsp from "fs/promises";
 import fs, {mkdirSync} from "fs";
 import path, {dirname} from "path";
 import {fileURLToPath} from 'url';
-
-// Make __dirname variable that tells parent dirctory.
-const __dirname = dirname(fileURLToPath(import.meta.url));
-// Path of clutter files.
-let filesPath = "clutter files";
-// Get all clutter files.
-let files = fs.readdirSync(filesPath);
 
 // organizeFile function move clutter files to organized folders.
 const organizeFile = (oldPath, newPath) => {
@@ -42,6 +36,19 @@ const organizeFile = (oldPath, newPath) => {
     console.log("File has been organized");
 };
 
+// Make __dirname variable that tells parent dirctory.
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// Path of clutter files.
+let filesPath = "clutter files";
+// Get all clutter files.
+let files = fs.readdirSync(filesPath);
+
+// If organized files folder is not exists then create it.
+if (fs.existsSync(`${__dirname}/organized files`) == false) {
+    // Make organized files folder.
+    await fsp.mkdir(`${__dirname}/organized files`);
+}
+
 // forEach loop for getting files one by one.
 files.forEach(element => {
     // Old path of files.
@@ -49,32 +56,15 @@ files.forEach(element => {
     // New path of files.
     let newPath = `${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}/${element}`;
 
-    // If organized files folder is not exists then run if block code otherwise run else block code.
-    if (fs.existsSync(`${__dirname}/organized files`) == false) {
-        // Make organized files folder.
-        fs.mkdirSync(`${__dirname}/organized files`);
-        // If (format name) folder is not exists then run if block code otherwise run else block code.
-        if (fs.existsSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`) == false) {
-            // Make (format name) folder.
-            mkdirSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`);
+    // If (format name) folder is not exists then run if block code otherwise run else block code.
+    if (fs.existsSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`) == false) {
+        // Make (format name) folder.
+        mkdirSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`);
 
-            // Move file from clutter files folder to (format name) folder with organizedFile function.
-            organizeFile(oldPath, newPath);
-        } else {
-            // Move file from clutter files folder to (format name) folder with organizedFile function.
-            organizeFile(oldPath, newPath);
-        }
+        // Move file from clutter files folder to (format name) folder with organizedFile function.
+        organizeFile(oldPath, newPath);
     } else {
-        // If (format name) folder is not exists then run if block code otherwise run else block code.
-        if (fs.existsSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`) == false) {
-            // Make (format name) folder.
-            mkdirSync(`${__dirname}/organized files/${path.extname(element).slice(1).toUpperCase()}`);
-            
-            // Move file from clutter files folder to (format name) folder with organizedFile function.
-            organizeFile(oldPath, newPath);
-        } else{
-            // Move file from clutter files folder to (format name) folder with organizedFile function.
-            organizeFile(oldPath, newPath);
-        }
+        // Move file from clutter files folder to (format name) folder with organizedFile function.
+        organizeFile(oldPath, newPath);
     }
 });
